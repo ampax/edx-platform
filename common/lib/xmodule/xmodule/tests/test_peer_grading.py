823aa10ccbb4bb7d3bcd24752575887d8ac0b969
Matt Drayer <mattdrayer@edx.org>
2015-10-15 13:54:40 -0400

    mattdrayer/edx-lint-violations: Fix missing docstrings

--- common/lib/xmodule/xmodule/tests/test_peer_grading.py
+++ common/lib/xmodule/xmodule/tests/test_peer_grading.py
@@ -1,3 +1,6 @@
+"""
+Test cases covering behaviors and workflows of the Peer Grading XBlock
+"""
 import unittest
 import json
 import logging
@@ -219,6 +222,9 @@ class PeerGradingModuleTest(unittest.TestCase, DummyModulestore):
         self.assertEqual(score_dict["score"], 0.0)
 
     def get_score(self, success, count_graded, count_required):
+        """
+        Returns the peer-graded score based on the provided graded/required values
+        """
         self.peer_grading.use_for_single_location_local = True
         self.peer_grading.graded = True
 
@@ -250,6 +256,9 @@ class PeerGradingModuleTest(unittest.TestCase, DummyModulestore):
 
 
 class MockPeerGradingServiceProblemList(MockPeerGradingService):
+    """
+    Mock object representing a set of peer-grading problems
+    """
     def get_problem_list(self, course_id, grader_id):
         return {'success': True,
                 'problem_list': [
@@ -349,6 +358,9 @@ class PeerGradingModuleLinkedTest(unittest.TestCase, DummyModulestore):
         return ScopeIds(None, None, self.problem_location, self.problem_location)
 
     def _create_peer_grading_descriptor_with_linked_problem(self):
+        """
+        Internal helper method to construct a peer grading XBlock
+        """
         # Initialize the peer grading module.
         system = get_test_descriptor_system()
 
